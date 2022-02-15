8527968912c7776e40a0cfc31975c95701e0e267
dewMyth <63700919+dewMyth@users.noreply.github.com>
2022-02-15 10:09:01 +0530

    Added mongodb connection to server

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,14 @@ dotenv.config();
 //Routes
 
 //Databasce Connection
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Mongo Cloud Database Connected Successfully!");
+  },
+  (err) => {
+    console.log("Error: ", err);
+  }
+);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started on port 5000");
