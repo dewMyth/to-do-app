4c5f0eb377165b703416a11b696b4abc4bbe70f5
dewMyth <63700919+dewMyth@users.noreply.github.com>
2022-02-15 12:26:54 +0530

    added user login - backend

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Register User
 router.post("/register", async (req, res) => {
@@ -13,12 +14,41 @@ router.post("/register", async (req, res) => {
       process.env.PASSWORD_SECRET_KEY
     ).toString(),
   });
-
-  console.log(newUser);
-
   try {
     const user = await newUser.save();
     res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Login User
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(404).json({ message: "User not found" });
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET_KEY
+    );
+    console.log(hashedPassword);
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    console.log(originalPassword);
+    originalPassword !== req.body.password &&
+      res.status(401).json({ message: "Wrong password" });
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECURITY_KEY,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
