b60d060855ea05e4b905827d173cb6710c044007
dewMyth <63700919+dewMyth@users.noreply.github.com>
2022-02-16 16:58:02 +0530

    Added delete todos

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -8,12 +8,22 @@ import ImageIcon from "@mui/icons-material/Image";
 import WorkIcon from "@mui/icons-material/Work";
 import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 
+import Badge from "@mui/material/Badge";
+import Stack from "@mui/material/Stack";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
+import axios from "axios";
+
 const Todo = ({ todo }) => {
-  console.log(todo);
   const { user } = useContext(AuthContext);
+
+  const config = {
+    headers: { token: `bearer ${user.accessToken}` },
+  };
+
   return (
     <List
       sx={{
@@ -30,6 +40,18 @@ const Todo = ({ todo }) => {
           </Avatar>
         </ListItemAvatar>
         <ListItemText primary={todo.title} secondary={todo.status} />
+        <Stack spacing={2} direction="row">
+          <Badge color="secondary">
+            <DeleteIcon
+              sx={{ color: "red" }}
+              onClick={async () => {
+                console.log(todo);
+                await axios.delete(`/api/todo/${user._id}/${todo._id}`, config);
+                window.location.reload();
+              }}
+            />
+          </Badge>
+        </Stack>
       </ListItem>
     </List>
   );
